4636dc229c81451e1f06695be15239090b113abe
Zinovev Ivan <izinovev77@gmail.com>
2026-03-05 01:31:23 +0300

    feat: use AppRoutes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import {AppRoutes} from "./routes/AppRoutes";
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <div className={`${process.env.REACT_APP_MAIN_THEME}`}>
+            <AppRoutes/>
+        </div>
+    );
 }
 
 export default App;
