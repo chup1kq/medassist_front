da37ce2e72121540a6183a7e678613524fff79a7
Zinovev Ivan <izinovev77@gmail.com>
2026-05-01 18:14:03 +0300

    feat: add auth API functions

---
+++ src/api/AuthApi.ts
@@ -0,0 +1,48 @@
+import { User } from "../data/auth/Auth";
+
+const AUTH_API = process.env.AUTH_API;
+
+export const getMe = async (): Promise<User> => {
+    const res = await fetch(AUTH_API + "/me", {
+        method: "GET",
+        credentials: "include",
+    });
+
+    if (!res.ok) throw new Error("Not authenticated");
+
+    return res.json();
+};
+
+export const login = async (data: {
+    username: string;
+    password: string;
+}): Promise<User> => {
+    const res = await fetch(AUTH_API + "/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+        credentials: "include",
+    });
+
+    if (!res.ok) throw new Error("Login failed");
+
+    return res.json();
+};
+
+export const logout = async (): Promise<void> => {
+    const res = await fetch(AUTH_API + "/logout", {
+        method: "POST",
+        credentials: "include",
+    });
+
+    if (!res.ok) throw new Error("Logout failed");
+};
+
+export const refresh = async (): Promise<void> => {
+    const res = await fetch(AUTH_API + "/refresh", {
+        method: "POST",
+        credentials: "include",
+    });
+
+    if (!res.ok) throw new Error("Refresh failed");
+};
