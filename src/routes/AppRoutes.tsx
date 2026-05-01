9fffb49d2cfa59bc6bc363e60ab8cb127461c717
Zinovev Ivan <izinovev77@gmail.com>
2026-05-01 11:36:03 +0300

    feat: add main and admin layouts to routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import AdminLayout from "../layout/AdminLayout";
 
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
+                <Route element={<MainLayout/>}>
 
+                </Route>
+
+                <Route element={<AdminLayout/>}>
+
+                </Route>
             </Routes>
         </BrowserRouter>
     )
