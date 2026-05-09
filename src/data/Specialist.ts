42e491af65fde46278fa665381aeebccb9db6020
Zinovev Ivan <izinovev77@gmail.com>
2026-05-10 01:22:35 +0300

    feat: add interfaces for specialist

--- src/data/Specialist.ts
+++ src/data/Specialist.ts
@@ -21,3 +21,27 @@ export interface Specialization {
 export interface SpecializationUpsert {
     name: string;
 }
+
+export interface SpecialistName {
+    id: number;
+    fullName: string;
+}
+
+export interface SpecialistCard {
+    id: number;
+    fullName: string;
+    description: string;
+    experienceYears: number;
+    photoUrl: string;
+    active: boolean;
+    specializations: Specialization[];
+}
+
+export interface SpecialistUpsertDto {
+    fullName: string;
+    description: string;
+    experienceYears: number;
+    photoUrl: string;
+    active: boolean;
+    specializationIds: number[];
+}
