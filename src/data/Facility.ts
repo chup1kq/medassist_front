9eea22efd1808dc8e14c83474132d91b0d65b629
Zinovev Ivan <izinovev77@gmail.com>
2026-05-10 01:15:02 +0300

    feat: add facility interface

---
+++ src/data/Facility.ts
@@ -0,0 +1,11 @@
+export interface FacilityName {
+    id: number;
+    name: string;
+}
+
+export interface Facility {
+    id: number;
+    name: string;
+    address: string;
+    description: string;
+}
